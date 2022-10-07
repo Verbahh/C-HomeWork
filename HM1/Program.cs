f7c9f5643c5f031701612b304af3bf6e8fd4fa85
Veronika <79639978662veron@gmail.com>
2022-10-07 17:28:15 +0300

    Initial commit

---
+++ HM1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if(n2 > max)
+{
+	max = n2;
+}
+
+if(n3 > max)
+{
+	max = n3;
+}
+
+Console.Write(max);
+*/
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= n)
+{
+	if(current % 2 == 0)
+	{
+		Console.Write(current + " ");
+	}
+    current++;
+}
+*/
