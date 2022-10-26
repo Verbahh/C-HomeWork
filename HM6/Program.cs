d5cbacaa9f49c5a6d1e6e37780ef5cb27378b3e6
Veronika <79639978662veron@gmail.com>
2022-10-26 07:05:44 +0300

    homework6

---
+++ HM6/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+Console.Write("Enter of numbers: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for(int i = 0; i < m; i++)
+{
+    Console.WriteLine("Input a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if(num > 0)
+        count++;
+}
+Console.WriteLine($"Positive numbers: {count}");
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+// y = k2 * x + b2 
+
+Console.WriteLine("Input a number b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input a number k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input a number b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input a number k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Intersection point coordinate: {x}, {y}");
+
+
+
